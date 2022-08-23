bb0ec90cb07563680865f4aba578d5914da5edbf
vishals33333 <111923439+vishals33333@users.noreply.github.com>
2022-08-23 17:50:09 -0500

    Add files via upload
    Simpler solution without hashmaps

--- solution/0000-0099/0001.Two Sum/Solution.java
+++ solution/0000-0099/0001.Two Sum/Solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            int v = nums[i];
-            int x = target - v;
-            if (m.containsKey(x)) {
-                return new int[]{m.get(x), i};
+    int [] res = new int [2];
+    for(int i = 0; i < nums.length; i ++){
+        int a = nums[i];
+        int b = target - a;
+        for(int j = i + 1; j < nums.length; j ++){
+            if(b == nums[j]){
+                res[0] = i;
+                res[1] = j;
             }
-            m.put(v, i);
         }
-        return null;
+       }
+       return res;
     }
 }
